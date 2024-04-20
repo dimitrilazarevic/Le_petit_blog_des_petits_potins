d07499b9fbcbf69a68c3db9dcb287ab43327b3ab
dlazarevic <lazarevic.dimitri@gmail.com>
2024-04-20 12:14:29 +0200

    J'ai voulu refaire une beauté à la mise en forme des routes... ça a pris du temps disons !

--- src/lib/server/routes/routes.js
+++ src/lib/server/routes/routes.js
@@ -6,21 +6,32 @@ const nodemailer = require('nodemailer');
 const { TRANSPORTER,EMAIL,WEBSITE_URL } = require('../config')
 
 //Pour générer un lien temporaire pour valider l'inscription
-const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-function generateString(length) {
-    let result = '';
+function generateString(length) 
+{
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    let result = '';
+
+    for ( let i = 0; i < length; i++ ) 
+    {
+        result += 
+        characters
+        .charAt
+        (Math.floor
+            (Math.random() * charactersLength)
+        );
     }
     return result;
 }
 
 const router = Router();
 
-function filterEmptyFields(object) {
-    for (const key in object) {
-        if (object[key] == '') {
+function filterEmptyFields(object) 
+{
+    for (const key in object) 
+    {
+        if (object[key] == '') 
+        {
             delete object[key];
         }
     }
@@ -29,73 +40,134 @@ function filterEmptyFields(object) {
 
 //Truc random
 router.get('/', (req, res) => {
-    res.send("Mes hommages.");
+    res
+    .send
+    ("Mes hommages.");
 })
 
 //Get tous les posts
 router.get('/get-posts', async (req, res) => {
-    try {
-        let postList = await Post.find();
-        if (postList == null || undefined) {
+    try 
+    {
+        let 
+        postList = 
+        await 
+        Post
+        .find(
+            {},
+            {},
+            {sort:{
+                createdAt:-1
+            }}
+        );
+
+        if (postList == null || undefined) 
+        {
             throw new Error("La liste n'a pas pu être chargée");
         }
-        res.status(200).json(postList);
+        res
+        .status(200)
+        .json(postList);
+
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        res
+        .status(500)
+        .json({ message: err.message });
     }
 })
 
 //Créer un post
 router.post('/create-post', async (req, res) => {
+
     let date = new Date();
     let filteredReq = filterEmptyFields(req.body);
     filteredReq.dateCreation=date;
-    try {
+
+    try 
+    {
         const newPost = new Post(filteredReq);
         const postSuccess = await newPost.save();
+
         if (!postSuccess) {
             throw new error("Post pas enregistré.")
         }
-        res.status(200).json(postSuccess)
-    } catch (err) {
-        res.status(500).json({ message: err.message })
+        res
+        .status(200)
+        .json(postSuccess)
+
+    }catch (err) 
+    {
+        res
+        .status(500)
+        .json({ message: err.message })
     }
 });
 
 //Supprimer un post
 router.delete('/delete-post/:id', async (req, res) => {
+
     let id = req.params.id;
-    try {
-        const deletedPost = await Post.findOneAndDelete({ _id: id });
-        if (deletedPost == null | undefined) {
+
+    try 
+    {
+        const 
+        deletedPost = 
+        await 
+        Post
+        .findOneAndDelete(
+            { _id: id }
+        );
+        if (deletedPost == null | undefined) 
+        {
             throw new Error("Pas pu suprimer chef.")
         }
-        res.status(200).json(deletedPost)
-    } catch (err) {
-        res.status(500).json({ message: err.message })
+        res
+        .status(200)
+        .json(deletedPost)
+
+    }catch (err) {
+        res
+        .status(500)
+        .json({ message: err.message })
     }
 })
 
 //Créer un utilisateur
 router.post('/create-user', async (req, res) => {
     try {
-        let [usersWithSameUsername,usersWithSameEmail] = await Promise.all([
-            User.findOne({username:req.body.username}),
-            User.findOne({email:req.body.email})
+        let [usersWithSameUsername,usersWithSameEmail] = 
+        await 
+        Promise.all([
+            User.findOne(
+                {username:req.body.username}
+            ),
+            User.findOne(
+                {email:req.body.email}
+            )
         ]);
-        let [usernameTaken,emailTaken]=[usersWithSameUsername,usersWithSameEmail].map((user)=>user=(user!==null));
-        if(usernameTaken){
+        let [usernameTaken,emailTaken]=
+        [usersWithSameUsername,usersWithSameEmail]
+        .map((user)=>{
+            user = (user !== null)
+        });
+        if(usernameTaken)
+        {
             throw new Error("Nom déjà pris !");
-        }else if(emailTaken){
+
+        }else if(emailTaken)
+        {
             throw new Error("Mail déjà pris !");
-        }else{
+
+        }else
+        {
             req.body.confirmationLink = generateString(128);
             let content = 
             '<h1>Bienvenue '+req.body.username+' !</h1>\
             <br/>\
             <a href='+WEBSITE_URL+'/confirm_registration/'+req.body.confirmationLink+'>Go to website</a>';
 
-            TRANSPORTER.sendMail(
+            TRANSPORTER.sendMail
+            (
                 {
                     from : EMAIL,
                     to : req.body.email,
@@ -110,13 +182,18 @@ router.post('/create-user', async (req, res) => {
             )
             const newUser = new User(req.body);
             const postSuccess = await newUser.save();
+
             if (!postSuccess) {
                 throw new error("User pas enregistré.");
             }
-            res.status(200).json(newUser);
+            res
+            .status(200)
+            .json(newUser);
         }
     } catch (err) {
-        res.status(422).json({ message: err.message });
+        res
+        .status(422)
+        .json({ message: err.message });
     }
 });
 
@@ -124,45 +201,81 @@ router.post('/create-user', async (req, res) => {
 router.post('/login',async (req,res)=>{
     try{
         let regExpMail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
-        let [userMatchingUsername,userMatchingEmail] = await Promise.all([
-            User.findOne({username:req.body.usernameoremail}),
-            User.findOne({email:req.body.usernameoremail})
+
+        let [userMatchingUsername,userMatchingEmail] = 
+        await 
+        Promise.all([
+            User.findOne(
+                {username:req.body.usernameoremail}
+            ),
+            User.findOne(
+                {email:req.body.usernameoremail}
+            )
         ]);
-        let userMatchingSearch=[userMatchingUsername,userMatchingEmail].find((el)=>el!=undefined);
-        let userExists = userMatchingSearch != undefined;
+
+        let userMatchingSearch =
+        [userMatchingUsername,userMatchingEmail]
+        .find((el)=>{
+            el != undefined
+        });
+
+        let userExists = (userMatchingSearch != undefined);
+
         if(!userExists){
-            if(req.body.usernameoremail.match(regExpMail)){
+            if(req.body.usernameoremail.match(regExpMail))
+            {
                 throw new Error("Pas d'utilisateur associé à ce mail.")
-            }else{
+
+            }else
+            {
                 throw new Error("Pas d'utilisateur associé à nom d'utilisateur.")
             }
         }
-        if(userMatchingSearch.password==req.body.password){
-            res.status(200).json(userMatchingSearch);
-        }else{
+
+        if(userMatchingSearch.password==req.body.password)
+        {
+            res
+            .status(200)
+            .json(userMatchingSearch);
+
+        }else
+        {
             throw new Error("Mauvais mot de passe...")
         }
-    }catch(err){
-        res.status(422).json({ message: err.message });
+
+    }catch(err)
+    {
+        res
+        .status(422)
+        .json({ message: err.message });
     }
 })
 
 //confirmer un utilisateur
 router.get('/confirm-registration/:confirmationLink', async (req, res) => {
-    try {
-        let user = await 
+    try 
+    {
+        let user = 
+        await 
         User
         .findOneAndUpdate(
-            {confirmationLink:req.params.confirmationLink},
-            {status:'user',confirmationLink:null},
-            {new:true}
+            {confirmationLink : req.params.confirmationLink},
+            {status : 'user',confirmationLink:null},
+            {new : true}
         );
         if(!user){
             throw new Error ('Lien inexistant')
         }
-        res.status(200).json({message:'Utilisateur confirmé avec succès',user:user});
-    } catch (err) {
-        res.status(404).json({ message: err.message });
+        res
+        .status(200)
+        .json({
+            message : 'Utilisateur confirmé avec succès',
+            user : user
+        });
+    }catch (err) {
+        res
+        .status(404)
+        .json({ message : err.message });
     }
 })
 
@@ -170,11 +283,13 @@ router.get('/confirm-registration/:confirmationLink', async (req, res) => {
 router.post('/forgotten-password',async (req,res)=>{
     try{
         let temporaryLink = generateString(128);
-        let userMatchingEmail = await 
+        let userMatchingEmail = 
+            await 
             User.findOneAndUpdate(
-                {email:req.body.email},
-                {confirmationLink:temporaryLink}
+                {email : req.body.email},
+                {confirmationLink : temporaryLink}
             );
+
         if(!userMatchingEmail){
             throw new Error("Pas d'utilisateur associé à ce mail.")
         }
@@ -197,17 +312,23 @@ router.post('/forgotten-password',async (req,res)=>{
                 }
             }
         )
-        res.status(200).json({message : "Un mail contenant un lien pour changer le mot de passe a été envoyé."})
+
+        res
+        .status(200)
+        .json({message : "Un mail contenant un lien pour changer le mot de passe a été envoyé."})
 
     }catch(err){
-        res.status(422).json({ message: err.message });
+        res
+        .status(422)
+        .json({ message: err.message });
     }
 })
 
 //Modifier un MDP
 router.post('/forgotten-password/:temporaryLink',async (req,res)=>{
     try{
-        let userChangingPassword = await 
+        let userChangingPassword = 
+            await 
             User.findOneAndUpdate(
                 {confirmationLink:req.params.temporaryLink},
                 {password:req.body.password,confirmationLink:null}
@@ -217,10 +338,14 @@ router.post('/forgotten-password/:temporaryLink',async (req,res)=>{
             throw new Error("Une erreur s'est produite...")
         }
 
-        res.status(200).json(userChangingPassword);
+        res
+        .status(200)
+        .json(userChangingPassword);
 
     }catch(err){
-        res.status(422).json({ message: err.message });
+        res
+        .status(422)
+        .json({ message: err.message });
     }
 })
 
